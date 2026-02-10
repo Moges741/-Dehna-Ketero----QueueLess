f7022ee7e5a3c8ebf15c2a6d42c676a4b7d5ecda
Moges741 <mogesse741@gmail.com>
2026-02-10 22:31:07 +0300

    queue table created

--- backend/config/initDb.js
+++ backend/config/initDb.js
@@ -56,6 +56,26 @@ CREATE TABLE IF NOT EXISTS tickets (
   FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE
 );
 `
+const createQueueTable = `
+CREATE TABLE IF NOT EXISTS queue_state (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+
+  office_id INT NOT NULL,
+  service_id INT NOT NULL,
+
+  current_ticket_number INT DEFAULT 0,
+  last_called_at DATETIME NULL,
+
+  queue_date DATE NOT NULL,
+
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+  UNIQUE(service_id, queue_date),
+
+  FOREIGN KEY (office_id) REFERENCES offices(id) ON DELETE CASCADE,
+  FOREIGN KEY (service_id) REFERENCES services(id) ON DELETE CASCADE
+);
+`
 
   db.query(createUsersTable, (err) => {
     if (err) {
@@ -77,11 +97,19 @@ CREATE TABLE IF NOT EXISTS tickets (
     } else {
       console.log("Services table ready");
     }
-  });  db.query(createTicketTable, (err) => {
+  });  
+  db.query(createTicketTable, (err) => {
     if (err) {
       console.error("Error creating tickets table:", err);
     } else {
       console.log("Tickets table ready");
+    }
+  });
+    db.query(createQueueTable, (err) => {
+    if (err) {
+      console.error("Error creating queue table:", err);
+    } else {
+      console.log("Queue table ready");
     }
   });
 };
