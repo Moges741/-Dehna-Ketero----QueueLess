26752d44e649935d24742c948f34baa262c5ad6d
Moges741 <mogesse741@gmail.com>
2026-02-12 11:38:25 +0300

    created langing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import {React, useState } from 'react'
 import './App.css'
 import NavBar from './components/common/navBar/NavBar'
+import Landing from './Landing'
 
 
 const App = () => {
   return (
     <div >
-      <NavBar/>
+      <Landing/>
     </div>
   )
 }
---
+++ frontend/src/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './components/common/navBar/NavBar'
+import Hero from './components/landing/Hero.jsx'
+
+const Landing = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Landing
